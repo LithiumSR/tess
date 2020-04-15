7923d8cbcc3432bb52427751926684e519e8e596
Leonardo Sarra <leonardosarra@outlook.com>
2020-04-15 02:16:45 +0200

    Make explicit some parameters of the lightgbm model

--- tess/model/feature_selection.py
+++ tess/model/feature_selection.py
@@ -21,7 +21,8 @@ class FeatureSelection:
         schema = Utils.get_available_feature_schema(self.data)
         X = [Utils.get_element_feature(schema, event.details, event.date) for event in self.data]
         Y = [Utils.get_target_function_value(self.data, event) for event in self.data]
-        gbm = lgb.LGBMRegressor()
+        gbm = lgb.LGBMRegressor(boosting_type='gbdt', num_leaves=31, max_depth=-1, learning_rate=0.1,
+                                n_estimators=100, objective='regression')
         gbm.fit(X, Y)
         features = gbm.feature_importances_
         for i in range(len(features)):
